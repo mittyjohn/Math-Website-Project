5886b504cb4e2b14bbba22e8cd72b1cc4e960410
ssmahaja <ssmahajabin@gmail.com>
2026-07-20 16:04:40 -0400

    changed to maven with Java 25

---
+++ backend/rest/src/main/java/math/rest/RestApplication.java
@@ -0,0 +1,15 @@
+package math.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
