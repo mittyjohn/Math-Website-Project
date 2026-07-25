e31482a28166c64fac6edb67e9c01eea7db1e6df
ange111306 <nguyenthienangelene@gmail.com>
2026-07-25 18:20:01 -0400

    Quiz Generator

--- frontend/math-web/app/page.tsx
+++ frontend/math-web/app/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './globals.css';
 import QuoteDisplay from './QuoteDisplay';
+import QuizGenerator from './QuizGenerator';
+
 export default function Home() {
   return (
     <div>
